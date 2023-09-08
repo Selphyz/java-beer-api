2d970122ebf239539f748a93546c22b2f214cfcd
Carlos Dominguez <carlos.dom13@hotmail.com>
2023-09-08 17:31:15 +0200

    csv beer file

--- src/main/java/com/example/demo/model/entities/Customer.java
+++ src/main/java/com/example/demo/model/entities/Customer.java
@@ -3,6 +3,8 @@ package com.example.demo.model.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -17,8 +19,11 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
+    @Column(length = 255)
+    private String email;
     @Version
     private Integer version;
     private String name;
